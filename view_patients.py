e7bb2c1624e4733c0ef05e38cbe8b2f41322d451
Blaise pascal <blaiseaganzepascal10@gmail.com>
2025-07-22 14:44:22 -0700

    created functions

--- view_patients.py
+++ view_patients.py
@@ -1,3 +1,9 @@
 from tkinter import *
 from database import get_all_patients
 
+def open_view_window():
+    win = Toplevel()
+    win.title("Liste des patients")
+    win.geometry("500x400")
+
+    
