a8664d99de75b85bc3ddfabfd2a5d4f84f85a2fa
Blaise pascal <blaiseaganzepascal10@gmail.com>
2025-07-22 14:45:26 -0700

    created functions

--- view_patients.py
+++ view_patients.py
@@ -6,4 +6,6 @@ def open_view_window():
     win.title("Liste des patients")
     win.geometry("500x400")
 
-    
+    rows = get_all_patients()
+    for row in rows:
+        Label(win, text=f"ID: {row[0]}, Nom: {row[1]}, Âge: {row[2]}, Sexe: {row[3]}, Maladie: {row[4]}, Contact: {row[5]}").pack()
