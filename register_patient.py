de280e1a01d1ca6c127f025fbefb95dbbba711dd
Blaise pascal <blaiseaganzepascal10@gmail.com>
2025-07-23 14:45:10 -0700

    created functions

---
+++ register_patient.py
@@ -0,0 +1,21 @@
+from tkinter import *
+from database import insert_patient
+
+def open_register_window():
+    win = Toplevel()
+    win.title("Enregistrer un patient")
+    win.geometry("300x300")
+
+    Label(win, text="Nom").pack()
+    name = Entry(win)
+    name.pack()
+
+    Label(win, text="Âge").pack()
+    age = Entry(win)
+    age.pack()
+
+    Label(win, text="Sexe").pack()
+    gender = Entry(win)
+    gender.pack()
+
+    
